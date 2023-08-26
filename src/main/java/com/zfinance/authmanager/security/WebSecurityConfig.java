5deb0f4e5b3a3b6fdb412bcd8ee7f77284c34ed5
Omar Abdullah <omar.amaghawry@gmail.com>
2023-08-26 21:04:52 +0300

    edit wweb security

--- src/main/java/com/zfinance/authmanager/security/WebSecurityConfig.java
+++ src/main/java/com/zfinance/authmanager/security/WebSecurityConfig.java
@@ -4,30 +4,34 @@ import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
+@Configuration
 @EnableWebSecurity
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+public class WebSecurityConfig {
 
 	@Autowired
 	private JwtAuthenticationEntryPoint jwtAuthenticationEntryPoint;
 	@Autowired
 	private JwtRequestFilter jwtRequestFilter;
 
-	@Override
-	protected void configure(HttpSecurity httpSecurity) throws Exception {
-		String[] resources = { "/auth/login" };
-		httpSecurity.csrf().disable().authorizeRequests().antMatchers(resources).permitAll().anyRequest()
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+		String[] resources = { "/auth/authorization" };
+		httpSecurity.csrf().disable().authorizeHttpRequests().antMatchers(resources).permitAll().anyRequest()
 				.authenticated().and().exceptionHandling().authenticationEntryPoint(jwtAuthenticationEntryPoint).and()
 				.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
+		return httpSecurity.build();
+
 	}
 
 	@Bean
